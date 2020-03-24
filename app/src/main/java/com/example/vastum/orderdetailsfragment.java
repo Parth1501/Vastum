2b7e02a703a114fe849500ef396fb97e215d9abc
tailorsmit <sjtailor27@gmail.com>
2020-03-24 11:49:53 +0530

    added recycler view in orderdetails tab in order activity

--- app/src/main/java/com/example/vastum/orderdetailsfragment.java
+++ app/src/main/java/com/example/vastum/orderdetailsfragment.java
@@ -8,10 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class orderdetailsfragment extends Fragment {
     View view;
-
+    RecyclerView recyclerView;
+    String order[]={"order1","order2","order3","order4","order5","order6"};
     orderdetailsfragment(){
 
     }
@@ -20,6 +23,9 @@ public class orderdetailsfragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.orderdetailsfragment,container,false);
+        recyclerView = view.findViewById(R.id.odrecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext(),LinearLayoutManager.VERTICAL,false));
+        recyclerView.setAdapter(new OdetailAdapter(this.getContext(),order));
         return view;
     }
 }
